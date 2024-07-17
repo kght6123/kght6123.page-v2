03e2f6975fbf4c43b182014637f8dd535d809e52
kght6123 <i.feeling.song+github@gmail.com>
2024-07-17 23:27:52 +0900

    update.

---
+++ theme.config.jsx
@@ -0,0 +1,37 @@
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © kght6123.
+      <a href="/feed.xml">RSS</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  ),
+  head: ({ title, meta }) => (
+    <>
+      {meta.description && (
+        <meta name="description" content={meta.description} />
+      )}
+      {meta.tag && <meta name="keywords" content={meta.tag} />}
+      {meta.author && <meta name="author" content={meta.author} />}
+    </>
+  ),
+  readMore: 'Read More →',
+  postFooter: null,
+  darkMode: false,
+  navs: [
+    {
+      url: 'https://github.com/shuding/nextra',
+      name: 'Nextra'
+    }
+  ]
+}

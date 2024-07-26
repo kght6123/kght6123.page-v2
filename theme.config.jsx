e0f17a23a1ad3672d6a5f8af3dc13d017d8c28e3
kght6123 <i.feeling.song+github@gmail.com>
2024-07-27 01:21:14 +0900

    update.

--- theme.config.jsx
+++ theme.config.jsx
@@ -43,8 +43,26 @@ export default {
       )}
       {meta.tag && <meta name="keywords" content={meta.tag} />}
       {meta.author && <meta name="author" content={meta.author} />}
+      <meta
+        property="og:image"
+        content={`https://placehold.jp/50/000000/ffffff/1200x600.png?text=${title}&css=%7B%22border-radius%22%3A%2215px%22%2C%22background%22%3A%22%20-webkit-gradient(linear%2C%20left%20top%2C%20left%20bottom%2C%20from(%23111111)%2C%20to(%23555555))%22%7D`}
+      />
     </>
   ),
+  useNextSeoProps: () => ({
+    // defaultTitle: "nextra-ogp-sample",
+    // titleTemplate: "nextra-ogp-sample",
+    // description: "nextra-ogp-sample",
+    twitter: {
+      handle: "@kght6123",
+      site: "@kght6123",
+      cardType: "summary_large_image",
+    },
+    // openGraph: {
+    //   url: "https://nextra-ogp-sample.vercel.app",
+    //   description: "nextra-ogp-sample",
+    // },
+  }),
   readMore: 'Read More →',
   dateFormatter: date => `Last updated at ${date.toDateString()}`,
   postFooter: null,
